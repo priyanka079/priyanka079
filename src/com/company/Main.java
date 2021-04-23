1dcecb72f5a7b35399c296ae7515cd330fbb4cb8
priyanka <priyanka.maguluri7@gmail.com>
2021-04-22 21:23:15 -0400

    first version

---
+++ src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int x=25;
+	    int y=4;
+	    int z;
+	    z=x%y;
+	    System.out.println("remainder of x and y is" +z);
+	    int w;
+	    w=x/y;
+	    System.out.println("quotient of x and y is" +w);
+    }
+}
