613c1624a1d6c61a0628be9fa7f4c25a57d1500c
priyanka <priyanka.maguluri7@gmail.com>
2021-04-25 14:32:55 -0400

    practice

---
+++ src/com/company/evenorodd.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class evenorodd {
+    public static void main(String[] args)
+    {
+        int x=23;
+        if(x%2==0)
+        {
+            System.out.println("x is even");
+        }
+        else
+        {
+            System.out.println("x is not even");
+        }
+    }
+}
